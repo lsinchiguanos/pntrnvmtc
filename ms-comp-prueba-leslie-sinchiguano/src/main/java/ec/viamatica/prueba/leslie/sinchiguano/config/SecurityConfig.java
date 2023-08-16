82f2f731867696ddbc1a2bccb521aaedc3eab4ff
Leslie Alexander Sinchiguano Saltos <69779736+lsinchiguanos@users.noreply.github.com>
2023-08-15 20:48:47 -0500

    ok
    ok

--- ms-comp-prueba-leslie-sinchiguano/src/main/java/ec/viamatica/prueba/leslie/sinchiguano/config/SecurityConfig.java
+++ ms-comp-prueba-leslie-sinchiguano/src/main/java/ec/viamatica/prueba/leslie/sinchiguano/config/SecurityConfig.java
@@ -4,27 +4,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-	@Bean
-	SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
 		return httpSecurity
-				.csrf(null)
-				.authorizeHttpRequests(auth -> {
-					auth.requestMatchers("/").permitAll();
-					auth.anyRequest().authenticated();
-				})
-				.sessionManagement(session -> {
-					session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
-				})
-				.httpBasic(null)
-				.and()				
+				.csrf(csrf ->
+						csrf
+						.disable())
+				.authorizeHttpRequests(authRequest ->
+					authRequest
+						.requestMatchers("/auth/**").permitAll()
+						.anyRequest().authenticated()
+					)
+				.formLogin(null)
 				.build();
 	}
-	
 }
